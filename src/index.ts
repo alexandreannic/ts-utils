416b663e8040f80b2a577c52100d1dd4a1bc443d
Alexandre Annic <alexandre.annic1@gmail.com>
2019-06-28 14:24:37 +0200

    Init countLine methods

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './validateObject/ValidateObject';
 export * from './validateObject/ValidateObjectDeep';
 export * from './validateObject/LogObject';
 export * from './elapsedTime/ElapsedTime';
+export * from './countLine/countLine';
