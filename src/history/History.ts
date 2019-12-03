03807e3f1e4152567cba88f1268546b7a392d90c
Jordan Munoz <jmunoz@mediarithmics.com>
2019-12-03 18:12:29 +0100

    All records keys must be string

--- src/history/History.ts
+++ src/history/History.ts
@@ -4,43 +4,44 @@ import stringify from 'csv-stringify/lib/sync';
 
 
 export interface DefaultRecord {
-    key: string;
-    date: Date;
+  key: string;
+  date: string;
+  [key: string]: string;
 }
 
 export class History<T extends DefaultRecord> {
 
-    private history: T[] = [];
+  private history: T[] = [];
 
-    constructor(private filePath: string) {
-        fs.ensureFileSync(this.filePath);
-        this.history = parse(fs.readFileSync(this.filePath), { columns: true }).map((a: any) => ({ ...a , date: new Date(a.date) }))
-        this.sortHistoryByDate();
-    }
+  constructor(private filePath: string) {
+    fs.ensureFileSync(this.filePath);
+    this.history = parse(fs.readFileSync(this.filePath), { columns: true });
+    this.sortHistoryByDate();
+  }
 
-    private flushHistory = () => {
-        this.sortHistoryByDate();
-        fs.writeFileSync(this.filePath, Buffer.from(stringify(this.history, { header: true })));
-    }
+  private flushHistory = () => {
+    this.sortHistoryByDate();
+    fs.writeFileSync(this.filePath, Buffer.from(stringify(this.history, { header: true })));
+  }
 
-    private sortHistoryByDate = () => {
-        this.history.sort((a, b) => a.date.getTime() - b.date.getTime());
-    }
+  private sortHistoryByDate = () => {
+    this.history.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  }
 
-    addRecord = (record: T) => {
-        this.history.push(record);
-        this.flushHistory();
-    }
+  addRecord = (record: T) => {
+    this.history.push(record);
+    this.flushHistory();
+  }
 
-    getLastRecord = (): T | undefined => {
-        return this.history[0];
-    }
+  getLastRecord = (): T | undefined => {
+    return this.history[0];
+  }
 
-    getRecordByKey = (key: string): T | undefined => {
-        return this.history.find(a => a.key === key);
-    }
+  getRecordByKey = (key: string): T | undefined => {
+    return this.history.find(a => a.key === key);
+  }
 
-    getHistory = () => {
-        return [...this.history];
-    }
- }
+  getHistory = () => {
+    return [...this.history];
+  }
+}
