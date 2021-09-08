a64e0c3662dddb39211b1a55bdae330a0c68b381
Alexandre Annic <alexandre.annic1@gmail.com>
2021-09-08 06:47:08 +0200

    Feature(ClipboardApi): initial version

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,15 +1,1 @@
-export * from './common/Common';
-export * from './common/CommonType';
-export * from './validateObject/ValidateObject';
-export * from './validateObject/ValidateObjectDeep';
-export * from './validateObject/LogObject';
-export * from './duration/Duration';
-export * from './prefixLogger/prefixLogger';
-export * from './progress/Progress';
-export * from './env/Env';
-export * from './env/EnvParser';
-export * from './lazy/Lazy';
-export * from './enum/Enum';
-export * from './mapPromise/MapPromise';
-
-
+export * from './clipboard/ClipboardApi'
