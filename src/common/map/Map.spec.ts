008c5c1d9ecbbc8bf335d6eec7ce27c53ad49a4e
Alexandre Annic <alexandre.annic1@gmail.com>
2022-06-14 18:43:29 +0200

    Map: allow multiple params

--- src/common/map/Map.spec.ts
+++ src/common/map/Map.spec.ts
@@ -3,20 +3,37 @@ import {map} from './Map'
 
 describe('Map', function () {
 
-  const obj = {
+  interface Obj {
     key: {
-      subKey: 2,
-      subKeyUndefined: undefined,
+      subString?: string | undefined
+      subNumber: number | undefined
+      subNumberUndefined: number | undefined
+    }
+  }
+
+  const obj: Obj = {
+    key: {
+      subString: 'a',
+      subNumber: 2,
+      subNumberUndefined: undefined,
     }
   }
 
   const square = (x: number) => x * x
 
   it('should prevent function computation at the 2nd call', async function () {
-    expect(map(obj.key.subKey, square)).eq(4)
+    expect(map(obj.key.subNumber, square)).eq(4)
   })
 
   it('should prevent function computation at the 2nd call', async function () {
-    expect(map(obj.key.subKeyUndefined, square)).eq(undefined)
+    expect(map(obj.key.subNumberUndefined, square)).eq(undefined)
+  })
+
+  it.only('should not call when one of the 2 args is undefined', async function () {
+    expect(map(obj.key.subNumber, obj.key.subNumberUndefined, ([a, b]) => a + b)).eq(undefined)
+  })
+
+  it.only('should call with 2 args', async function () {
+    expect(map(obj.key.subString, obj.key.subNumber, ([a, b]) => a + b)).eq('a2')
   })
 })
--- src/common/map/Map.ts
+++ src/common/map/Map.ts
@@ -1,3 +1,10 @@
-export const map = <T, R>(t: T | undefined, fn: (t: T) => R): R | undefined => {
-  return t !== undefined ? fn(t) : undefined
+export type Map = {
+  <A, R>(a: A | undefined, fn: (_: A) => R): R | undefined
+  <A, B, R>(a: A | undefined, b: B | undefined, fn: (_: [A, B]) => R): R | undefined
+  // <A, B, C, R>(a: [A | undefined, B | undefined, C | undefined], fn: (_: [A, B, C]) => R): R | undefined
+  // <A, B, C, D, R>(a: [A | undefined, B | undefined, C | undefined, D | undefined], fn: (_: [A, B, C, D]) => R): R | undefined
+}
+export const map: Map = (...args: any[]) => {
+  const fn = args.pop()
+  return args.every(_ => _ !== undefined) ? fn(args) : undefined
 }
